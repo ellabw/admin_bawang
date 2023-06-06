26ce185712d4b3ee9569fbf6dcf88c060a14c752
ellabw <111202012716@mhs.dinus.ac.id>
2023-06-06 15:08:17 +0700

    first

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+export {default as Button} from './Button';
+export {default as Sidebar} from './Sidebar';
+export {default as Navbar} from './Navbar';
+export {default as Footer} from './Footer';
+export {default as Header} from './Header';
+export {default as Cart} from './Cart';
+export {default as Chat} from './Chat';
+export {default as UserProfile} from './UserProfile';
+export {default as Notification } from './Notification';
+export {default as LineChart} from './Chart/LineChart';
+export {default as Pie} from './Chart/Pie';
+export {default as SparkLine} from './Chart/SparkLine';
+export {default as Stacked} from './Chart/Stacked';
+export {default as ThemeSettings} from './ThemeSettings';
+export {default as ChartsHeader} from './ChartsHeader';
